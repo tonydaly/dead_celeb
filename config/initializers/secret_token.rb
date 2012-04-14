d4e7ff64bd592b4d41963ba79dfa940fef9647fb
Mark Burns <markthedeveloper@gmail.com>
2012-04-14 18:10:24 +0100

    removed rails app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DeadCeleb::Application.config.secret_token = '2ce65219adfba50932b6d229d3e14409704111601ededdebfa5b3723239852f640bf5ee0aa639975cff672b0dae58a7471f746aee486355a7756a9eb7a373e02'
