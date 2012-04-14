304436149a7772afabc03b9c5ef894a912bfba11
Mark Burns <markthedeveloper@gmail.com>
2012-04-14 18:08:51 +0100

    working streaming

--- dead_celeb_em/em_twitter.rb
+++ dead_celeb_em/em_twitter.rb
@@ -3,72 +3,69 @@ require 'em-http'
 require 'em-http-oauth-request'
 require 'oauth'
 require 'json'
+require 'oauth/client/em_http'
+require 'awesome_print'
 
 class TwitterStream
-
-  def initialize(term)
-    @term = term
-    @buffer = ""
-    listen
+  def initialize term
+    listen term
   end
-
-  def on_tweet
-    lambda{ |o| puts o; puts o['text'] }
-  end
-
 
   private
-  def listen
-    http = EventMachine::HttpRequest.new(filter_url)
 
-    oauth_helper = OAuth::Client::Helper.new(http, oauth_params)
+  def listen term
+    http = EventMachine::HttpRequest.new('https://stream.twitter.com/1/statuses/filter.json').
+     post(:body=>{"track"=>"is dead"},
+      :head => {"Content-Type" => "application/x-www-form-urlencoded"},
+      :timeout => -1) do |client|
+      twitter_oauth_consumer.sign!(client, twitter_oauth_access_token)
+    end
 
-    http.options[:head] = (http.options[:head] || {}).merge!({"Authorization" => oauth_helper.header})
-    http.options.merge!({ :query => { "track" => @term }})
+    http.errback    { puts "oops" }
+    http.disconnect { puts "oops, dropped connection?" }
 
-    parser = Yajl::Parser.new.tap { |p| p.on_parse_complete = on_tweet }
 
-    http.stream { |chunk| parser << chunk }
+    buffer = ""
+
+    http.stream do |chunk|
+      buffer += chunk
+      while line = buffer.slice!(/.+\r?\n/)
+        line = JSON.parse line
+        ap line["text"]
+      end
+    end
   end
 
-  def oauth_params
-    {:consumer => consumer, :token => oauth[:access_token]}
+  def twitter_oauth_consumer
+    @twitter_oauth_consumer ||= OAuth::Consumer.new(consumer_key, consumer_secret, :site => "http://twitter.com")
+  end
+
+  def twitter_oauth_access_token
+    @twitter_oauth_access_token ||= OAuth::AccessToken.new(twitter_oauth_consumer, access_key, access_secret)
   end
 
   def filter_url
    'http://stream.twitter.com/1/statuses/filter.json'
   end
 
-  def home_timeline_url
-    'http://api.twitter.com/1/statuses/home_timeline.json'
+  #consumer
+  def consumer_key
+   '6uM57iOvtBvbeGqpFeuTQ'
   end
 
-  def oauth
-    {
-      :consumer_token    => '6uM57iOvtBvbeGqpFeuTQ',
-      :consumer_secret => 'EJ1ACfTVe0BozKh1mT0HmSSSLbyaApNMQHROwW4',
-
-      :access_token      => '28869726-GBGpzaPFoMPXlIeBJZk4ubuc7rmUmXRcAtgtPVE8T',
-      :access_secret   => 'FRpwYXjsvpu1OicjckuEY7Sao7BdjqpeRqpKhLG48'
-    }
+  def consumer_secret
+    'EJ1ACfTVe0BozKh1mT0HmSSSLbyaApNMQHROwW4'
   end
 
-  def consumer
-    OAuth::Consumer.new(oauth[:consumer_token], oauth[:consumer_secret],
-                        :site => 'http://twitter.com')
+  #access
+  def access_key
+    '28869726-GBGpzaPFoMPXlIeBJZk4ubuc7rmUmXRcAtgtPVE8T'
   end
 
-  def access_token
-    OAuth::AccessToken.new(consumer, oauth[:access_token], oauth[:access_secret])
+  def access_secret
+    'FRpwYXjsvpu1OicjckuEY7Sao7BdjqpeRqpKhLG48'
   end
-
-
 end
 
 
-EM.run {
-  stream = TwitterStream.new(ARGV[0] || 'dead')
-  stream.on_tweet { |tweet|
-    puts "New tweet: #{tweet}"
-  }
-}
+EM.run { TwitterStream.new(ARGV[0] || 'dead') }
