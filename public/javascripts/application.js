fb206594ee50d912ab6d18c3267ea6cb7a587b2f
ijtaba <ijtaba@ijtaba-laptop.(none)>
2012-04-14 20:35:35 +0100

    changed website to public

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,33 @@
-// Put your application scripts here
+var scrollTweetID = 1;
+function scrollTweets(){
+	//fetch tweet
+	deathTweet = "Justin bieber is dead!!";
+	fatalatyTweet = "Kanye West";
+	wishTweet = "Donald Trump";
+	
+	if(scrollTweetID>=3){	
+		//slide down
+		$("#"+scrollTweetID).slideDown();
+		$("#"+scrollTweetID+1).slideDown();
+		$("#"+scrollTweetID+2).slideDown();
+	}
+	
+	//control box size
+	if(scrollTweetID>15){
+		$("#"+(scrollTweetID-15)).remove();
+		$("#"+(scrollTweetID-14)).remove();
+		$("#"+(scrollTweetID-13)).remove();
+	}
+	
+	scrollTweetID = scrollTweetID + 3 ;
+	
+	$("#RecentDeaths").prepend("<p id=\"" + scrollTweetID + "\">"+deathTweet+"</p>");
+	$("#fatalHopefulls").prepend("<p id=\"" + scrollTweetID +1 + "\">"+fatalatyTweet+"</p>");
+	$("#wishList").prepend("<p id=\"" + scrollTweetID +2 + "\">"+wishTweet+"</p>");
+	
+	//hscrollTweetIDe new tweet
+	$("#"+scrollTweetID).hide();
+	setTimeout(scrollTweets, 1000);
+}
+//run the bitch;
+scrollTweets();
