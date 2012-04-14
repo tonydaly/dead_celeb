9a1e1266d2896a8ae1e7756b9fb6987151b962b9
Mark Burns <markthedeveloper@gmail.com>
2012-04-15 00:42:43 +0100

    slide down js

--- app/models/death_stream.rb
+++ app/models/death_stream.rb
@@ -4,6 +4,7 @@ class DeathStream
 
     stream.listen("is dead") do |tweet|
       EventMachine.defer do
+        begin
         puts "tweet received #{tweet}"
         count = $celebrities[celeb.name][:count] += 1
         puts "death count #{count}"
@@ -13,7 +14,10 @@ class DeathStream
         if count > 10
           ap "#{celeb.name} is dead - time to throw a party"
           $celebrities[celeb.name][:count] = 0
+        end
 
+        rescue StandardError => e
+          puts "Some kind of error in EventMachine.defer #{e.message} #{e.inspect}"
         end
       end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,35 +1,11 @@
-var scrollTweetID = 1;
-function scrollTweets(){
-	//fetch tweet
-	deathTweet = "Justin bieber is dead!!";
-	fatalatyTweet = "Kanye West";
-	wishTweet = "Donald Trump";
-	
-	if(scrollTweetID>=3){	
-		//slide down
-		$("#"+scrollTweetID).slideDown();
-		$("#"+(scrollTweetID+1)).slideDown();
-		$("#"+(scrollTweetID+2)).slideDown();
-	}
-	
-	//control box size
-	if(scrollTweetID>15){
-		$("#"+(scrollTweetID-15)).remove();
-		$("#"+(scrollTweetID-14)).remove();
-		$("#"+(scrollTweetID-13)).remove();
-	}
-	
-	scrollTweetID = scrollTweetID + 3 ;
-	
-	$("#RecentDeaths").prepend("<p id=\"" + scrollTweetID + "\">"+deathTweet+"</p>");
-	$("#fatalHopefulls").prepend("<p id=\"" + (scrollTweetID +1) + "\">"+fatalatyTweet+"</p>");
-	$("#wishList").prepend("<p id=\"" + (scrollTweetID +2) + "\">"+wishTweet+"</p>");
-	
-	//hscrollTweetIDe new tweet
-	$("#"+scrollTweetID).hide();
-	$("#"+(scrollTweetID+1)).hide();
-	$("#"+(scrollTweetID+2)).hide();	
-	setTimeout(scrollTweets, 1000);
-}
-//run the bitch;
-scrollTweets();
+var scrollTweets = function(deathTweet){
+  var deathTweets = [];
+
+  scrollTweets = function(deathTweet){
+    deathTweets.shift();
+    deathTweets.push(deathTweet);
+
+    var p= "<p>" + deathTweet + "</p>";
+    $("#recent-deaths").prepend(p);
+  };
+};
