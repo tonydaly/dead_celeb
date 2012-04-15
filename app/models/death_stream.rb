770cd863cfbb772725e8c35c990b1c31c8626161
Tony Daly <tony.daly@me.com>
2012-04-15 11:49:17 +0100

    Extra info for each death tweet and pretty display.

--- app/models/death_stream.rb
+++ app/models/death_stream.rb
@@ -19,7 +19,12 @@ class DeathStream
             Pusher['dead_celebs'].trigger_async('death',
                 { name: celeb.name,
                   url: "/celebrities/#{celeb.id}",
-                  tweet: tweet})
+                  tweet: tweet["text"],
+                  user: {
+                    name: tweet["user"]["name"],
+                    profile: tweet["user"]["profile_image_url"]
+                  }
+                })
           end
 
         rescue StandardError => e
