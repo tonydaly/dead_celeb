25f42ef2b0690a201e7f44e66dfc6fb0521a7028
Mark Burns <markthedeveloper@gmail.com>
2012-04-15 01:48:06 +0100

    save dead attribute

--- app/models/death_stream.rb
+++ app/models/death_stream.rb
@@ -5,16 +5,17 @@ class DeathStream
     stream.listen("is dead") do |tweet|
       EventMachine.defer do
         begin
-        puts "tweet received #{tweet}"
-        count = $celebrities[celeb.name][:count] += 1
-        puts "death count #{count}"
+          count = $celebrities[celeb.name][:count] += 1
+          puts "death count #{count} #{tweet}"
 
 
-        if count > 10
-          ap "#{celeb.name} is dead - time to throw a party"
-          $celebrities[celeb.name][:count] = 0
-          Pusher['dead_celebs'].trigger_async('death', {message: "#{celeb.name}: #{tweet}"})
-        end
+          if count > 10
+            ap "#{celeb.name} is dead - time to throw a party"
+            $celebrities[celeb.name][:count] = 0
+            celeb.update_attribute dead: true
+
+            Pusher['dead_celebs'].trigger_async('death', {message: "#{celeb.name}: #{tweet}"})
+          end
 
         rescue StandardError => e
           puts "Some kind of error in EventMachine.defer #{e.message} #{e.inspect}"
